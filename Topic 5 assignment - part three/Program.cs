03c90195878855ceadb1effdf3a5861c34bd5b8f
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-21 14:26:18 -0500

    Add project files.

---
+++ Topic 5 assignment - part three/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5_assignment___part_three
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
